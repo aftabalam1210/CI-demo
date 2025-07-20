6a98d3a0ab519f8058093b615e887ed09a61a2f6
aftabalam1210 <zraone1210@gmail.com>
2025-07-20 10:41:35 +0530

    flake8 fix and yaml

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-# we are going to create a function to test 
+# We are going to create a function to test
 
 # 1. string to lower
 def lower_case(data):
     return data.lower()
 
-#2. upper case
+
+# 2. upper case
 def upper_case(data):
     return data.upper()
 
-# 3. print length 
-def lenght_string(data):
-    return len(data) 
+
+# 3. print length
+def length_string(data):
+    return len(data)
