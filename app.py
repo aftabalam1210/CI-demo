db54716b3c27309474e2c342b517232eca90096f
aftabalam1210 <zraone1210@gmail.com>
2025-07-20 10:27:48 +0530

    checking linting in code

---
+++ app.py
@@ -0,0 +1,13 @@
+# we are going to create a function to test 
+
+# 1. string to lower
+def lower_case(data):
+    return data.lower()
+
+#2. upper case
+def upper_case(data):
+    return data.upper()
+
+# 3. print length 
+def lenght_string(data):
+    return len(data) 
